512b90082a439092864519febba856644c7ba76f
Randy J. Hunt <randy@wearesupercorp.com>
2009-09-01 11:57:17 -0400

    change remaining references to Makers Market to MARKET_NAME config variable. Change Mohawk name to Felt & Wire

--- config/market_config.rb
+++ config/market_config.rb
@@ -1,7 +1,7 @@
 #this is a test of git merge
 MARKET_HELP_EMAIL = 'help@feltandwireshop.com'
 MARKET_LETTER_CLOSING = 'The Felt and Wire Shop'
-MARKET_NAME = 'Felt and Wire'
+MARKET_NAME = 'Felt & Wire'
 MARKET_DOMAIN_NAME = 'feltandwireshop.com'
 MARKET_AUTH_LOGIN = '7mhDd6E5VS2n'
 MARKET_AUTH_TRANS_ID = '7DmG6n53b8J897q7'
