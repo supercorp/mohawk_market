74e15101182fe5a3e64533db460cf8ee15bc818a
Eric Draut <edraut@gmail.com>
2009-09-02 15:01:58 -0400

    added market file for collection thumbnail sizes

---
+++ app/models/market_files/market_collection_market.rb
@@ -0,0 +1,8 @@
+module MarketCollectionMarket
+  BANNER_THUMBNAIL_SIZES = { :full_width => "866x100>"}
+  ICON_THUMBNAIL_SIZES = { :thumb_65 => "65x65#",
+                           :thumb_69 => "69x69#",
+                           :thumb_110 => "110x110#",
+                           :thumb_151 => "151x151#",
+                           :thumb_192 => "192x192#"}
+end
