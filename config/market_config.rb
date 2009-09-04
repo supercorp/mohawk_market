97e3805fcd25194ec46f513fa6ad29724b7e9f82
Eric Draut <edraut@gmail.com>
2009-09-04 11:58:56 -0400

    added market billing name and address to market config

--- config/market_config.rb
+++ config/market_config.rb
@@ -2,6 +2,9 @@
 MARKET_HELP_EMAIL = 'help@feltandwireshop.com'
 MARKET_LETTER_CLOSING = 'The Felt and Wire Shop'
 MARKET_NAME = 'Felt & Wire'
+MARKET_BILLING_NAME = "Mohawk Fine Papers Inc."
+MARKET_BILLING_ADDRESS = "465 Saratoga Street
+Cohoes, New York 12047"
 MARKET_DOMAIN_NAME = 'feltandwireshop.com'
 MARKET_AUTH_LOGIN = '7mhDd6E5VS2n'
 MARKET_AUTH_TRANS_ID = '7DmG6n53b8J897q7'
