3f5ff3ceb1d1d3b346727dee9ad581ffa42ef591
Randy J. Hunt <randy@citizenscholar.com>
2009-09-08 12:26:51 -0400

    mohawk, revised collection image sizes in model and solidified public collection intro and header MU

--- app/models/market_files/market_collection_market.rb
+++ app/models/market_files/market_collection_market.rb
@@ -1,11 +1,7 @@
 module MarketCollectionMarket
-  BANNER_THUMBNAIL_SIZES = { :full_width => "866x100>",
-                             :preview => "752x87>"}
-  BANNER_SIZE_HELP_TEXT = "upload a colleciton banner image, the optimal size is 752 x 87 px"
-  ICON_THUMBNAIL_SIZES = { :thumb_65 => "65x65#",
-                           :thumb_69 => "69x69#",
-                           :thumb_110 => "110x110#",
-                           :thumb_151 => "151x151#",
-                           :thumb_192 => "192x192#"}
+  BANNER_THUMBNAIL_SIZES = { :full_width => "483x280>",
+                             :preview => "483x280>"}
+  BANNER_SIZE_HELP_TEXT = "upload a colleciton banner image, the optimal size is 483 x 280 px"
+  ICON_THUMBNAIL_SIZES = { :full_width => "237x36>"}
   ICON_SIZE_HELP_TEXT = "upload a colleciton icon image, the optimal size is 237 x 36 px"
 end
