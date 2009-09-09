6b1af84bf2dc2e61829bc79a356e850dca9b8c23
Randy J. Hunt <randy@wearesupercorp.com>
2009-09-09 10:36:22 -0400

    rake git:commit[-a] m=removed

--- config/market_config.rb
+++ config/market_config.rb
@@ -8,4 +8,3 @@ Cohoes, New York 12047"
 MARKET_DOMAIN_NAME = 'feltandwireshop.com'
 MARKET_AUTH_LOGIN = '7mhDd6E5VS2n'
 MARKET_AUTH_TRANS_ID = '7DmG6n53b8J897q7'
-MARKET_COLLECTION_HEADER_TEXT = 'Explore Felt &amp; Wire &raquo;'
