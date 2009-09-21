b66295ef8c03b0ffe2db3df7904d3dfd35249b48
Randy J. Hunt <randy@wearesupercorp.com>
2009-09-21 13:36:49 -0400

    core: addition of product description help text in edit product info view, mohawk: copy for product description help text in market product model

--- app/models/market_files/product_market.rb
+++ app/models/market_files/product_market.rb
@@ -1,3 +1,4 @@
 module ProductMarket
   CLASS_DISPLAY_NAME = "Product"
+  PRODUCT_DESCRIPTION_HELP_TEXT = "Enter a brief description of the product here. This is your opportunity to describe what can&rsquo;t be seen in the product photographs. We kindly request that you only identify Mohawk-brand paper by name. You&rsquo;re welcome to describe the weight, finish, and quality of the paper."
 end
