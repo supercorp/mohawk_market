a9252a868c6c7f458d21667603c7952b3ff0d991
Eric Draut <edraut@gmail.com>
2009-09-28 19:43:14 -0400

    fixed mailer_config.rb

--- config/mailer_config.rb
+++ config/mailer_config.rb
@@ -1,14 +1,13 @@
 ActionMailer::Base.smtp_settings = {
   :address  => "smtp.gmail.com",
   :port => 587,
-  :user_name => 'mohawk.cc.dev@wearesupercorp.com',
+  :user_name => 'do-not-reply@feltandwireshop.com',
   :password => '233N0rm4n',
   :authentication => :plain
 }
-ActionMailer::Base::ORDER_FROM_ADDR = 'mohawk.cc.staging@wearesupercorp.com'
-ActionMailer::Base::ALERTS_FROM_ADDR = 'mohawk.cc.staging@wearesupercorp.com'
-ActionMailer::Base::BILLING_FROM_ADDR = 'mohawk.cc.staging@wearesupercorp.com'
-ActionMailer::Base::MESSAGE_FROM_ADDR = 'mohawk.cc.staging@wearesupercorp.com'
-ActionMailer::Base::WEB_SITE_URL = 'http://localhost:3000'
-ActionMailer::Base::FROM_ADDR = 'mohawk.cc.dev@wearesupercorp.com'
-ActionMailer::Base::SETUP_EMAIL_SUBJECT = "Sample CC dev site new account"
+ActionMailer::Base::ORDER_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
+ActionMailer::Base::ALERTS_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
+ActionMailer::Base::BILLING_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
+ActionMailer::Base::MESSAGE_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
+ActionMailer::Base::FROM_ADDR = 'do-not-reply@feltandwireshop.com'
+ActionMailer::Base::SETUP_EMAIL_SUBJECT = "[Felt and Wire Shop]"
