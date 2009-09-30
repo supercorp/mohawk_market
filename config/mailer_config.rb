10c1a03f5f1d23e0dc7e9d78e67923c8636b3f75
Eric Draut <edraut@gmail.com>
2009-09-30 13:23:57 -0400

    fixed mailer config

--- config/mailer_config.rb
+++ config/mailer_config.rb
@@ -2,12 +2,12 @@ ActionMailer::Base.smtp_settings = {
   :address  => "smtp.gmail.com",
   :port => 587,
   :user_name => 'do-not-reply@feltandwireshop.com',
-  :password => '233N0rm4n',
+  :password => 'feltandw1re',
   :authentication => :plain
 }
-ActionMailer::Base::ORDER_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
-ActionMailer::Base::ALERTS_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
-ActionMailer::Base::BILLING_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
-ActionMailer::Base::MESSAGE_FROM_ADDR = 'do-not-reply@feltandwireshop.com'
+ActionMailer::Base::ORDER_FROM_ADDR = 'orders@feltandwireshop.com'
+ActionMailer::Base::ALERTS_FROM_ADDR = 'alerts@feltandwireshop.com'
+ActionMailer::Base::BILLING_FROM_ADDR = 'billing@feltandwireshop.com'
+ActionMailer::Base::MESSAGE_FROM_ADDR = 'messages@feltandwireshop.com'
 ActionMailer::Base::FROM_ADDR = 'do-not-reply@feltandwireshop.com'
 ActionMailer::Base::SETUP_EMAIL_SUBJECT = "[Felt and Wire Shop]"
