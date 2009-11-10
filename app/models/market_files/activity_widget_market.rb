e4c07a5cfd8fffb7aeaa7c4ba9f85e4d23ca8102
Eric Draut <edraut@gmail.com>
2009-11-09 21:01:17 -0500

    added market-specific widget truncation lengths

---
+++ app/models/market_files/activity_widget_market.rb
@@ -0,0 +1,3 @@
+module ActivityWidgetMarket
+  TRUNCATION_LENGTH = {'half' => 100, 'full' => 200}
+end
---
+++ app/models/market_files/market_content_widget_market.rb
@@ -0,0 +1,3 @@
+module MarketContentWidgetMarket
+  TRUNCATION_LENGTH = {'tall' => 100, 'square' => 100, 'wide_2' => 150, 'wide_3' => 200, 'wide_4' => 250}
+end
