5be651c32c547012d4a7eb99b88ae1c981662c3b
Randy J. Hunt <randy@citizenscholar.com>
2009-11-11 16:44:30 -0500

    mohawk widget image size help text added

--- app/models/market_files/widget_market.rb
+++ app/models/market_files/widget_market.rb
@@ -8,6 +8,9 @@ module WidgetMarket
                      :widget_wide_3 => '729x238#',
                      :widget_wide_4 => '975x238#',
                      :widget_tall => '238'}
-  IMAGE_SIZE_HELP_TEXT = "Images should be..."
-  
+  IMAGE_SIZE_HELP_TEXT = {'square' => "238px x 238px",
+                         'tall' => "238px wide and any height",
+                         'wide_2' => "483px x 238px",
+                         'wide_3' => "729px x 238px",
+                         'wide_4' => "975px x 238px"} 
 end
