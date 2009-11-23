74137c6eceb508dcbae7587908bc22debc93fa5c
Randy J. Hunt <randy@citizenscholar.com>
2009-11-23 16:34:52 -0500

    market name changed from Felt & Wire to Felt & Wire Shop

--- config/market_config.rb
+++ config/market_config.rb
@@ -2,7 +2,7 @@
 MARKET_HELP_EMAIL = 'help@feltandwireshop.com'
 MARKET_ORDER_EMAIL = 'orders@feltandwireshop.com'
 MARKET_LETTER_CLOSING = 'The Felt and Wire Shop'
-MARKET_NAME = 'Felt & Wire'
+MARKET_NAME = 'Felt & Wire Shop'
 MARKET_BILLING_NAME = "Mohawk Fine Papers Inc."
 MARKET_BILLING_ADDRESS = "465 Saratoga Street
 Cohoes, New York 12047"
