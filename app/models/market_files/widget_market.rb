3c7792965af4d384c7d7c2c014b8fa6e4752a610
Eric Draut <edraut@gmail.com>
2009-12-18 19:13:18 -0500

    updated mohawk market widget thumbnails to use new activity_widget thumbnail instead of old activity_widget_half and _full thumbnails

--- app/models/market_files/widget_market.rb
+++ app/models/market_files/widget_market.rb
@@ -1,7 +1,6 @@
 module WidgetMarket
   THUMBNAIL_SIZES = {:widget_thumb => '69x69#',
-                     :activity_widget_full => '237x196#',
-                     :activity_widget_half => '96x96#',
+                     :activity_widget => '96x96#',
                      :market_widget_square => '96x96#',
                      :widget_square => '238x238#',
                      :widget_wide_2 => '483x238#',
