c344bbfa9483994f925db7e2e21d5b282e8356d4
Randy J. Hunt <randy@citizenscholar.com>
2010-01-01 15:09:57 -0500

    market content widget link styling, font size adjustments, and liner width adjustments, truncation set temporarly to size square

--- app/models/market_files/market_content_widget_market.rb
+++ app/models/market_files/market_content_widget_market.rb
@@ -1,3 +1,3 @@
 module MarketContentWidgetMarket
-  TRUNCATION_LENGTH = {'tall' => 100, 'square' => 100, 'wide_2' => 150, 'wide_3' => 200, 'wide_4' => 250}
+  TRUNCATION_LENGTH = {'tall' => 100, 'square' => 140, 'wide_2' => 150, 'wide_3' => 200, 'wide_4' => 250}
 end
