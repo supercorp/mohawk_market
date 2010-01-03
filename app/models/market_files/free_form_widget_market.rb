994e7610f6d7faa976c44cc995f27337814642aa
Eric Draut <edraut@gmail.com>
2010-01-03 13:53:24 -0500

    moved image size help text from widget_market into free_form_image_market

--- app/models/market_files/free_form_widget_market.rb
+++ app/models/market_files/free_form_widget_market.rb
@@ -4,4 +4,9 @@ module FreeFormWidgetMarket
                      :widget_wide_3 => '729x238#',
                      :widget_wide_4 => '975x238#',
                      :widget_tall => '238'}.merge Widget::THUMBNAIL_SIZES
+  IMAGE_SIZE_HELP_TEXT = {'square' => "238px x 238px",
+                        'tall' => "238px wide and any height",
+                        'wide_2' => "483px x 238px",
+                        'wide_3' => "729px x 238px",
+                        'wide_4' => "975px x 238px"} 
 end
--- app/models/market_files/widget_market.rb
+++ app/models/market_files/widget_market.rb
@@ -1,9 +1,4 @@
 module WidgetMarket
   THUMBNAIL_SIZES = {:widget_thumb => '69x69#'}
-  IMAGE_SIZE_HELP_TEXT = {'square' => "238px x 238px",
-                         'tall' => "238px wide and any height",
-                         'wide_2' => "483px x 238px",
-                         'wide_3' => "729px x 238px",
-                         'wide_4' => "975px x 238px"} 
   PANEL_WIDTH = {1 => 246, 2 => 492, 3 => 738, 4 => 984}
 end
