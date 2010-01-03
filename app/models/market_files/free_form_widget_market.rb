a7e9e342e6b9330e9e72d749fc08a1f957577c45
Randy J. Hunt <randy@citizenscholar.com>
2010-01-03 13:36:33 -0500

    adjust freeform widget thumbnail sizes to reflect values needed for product panel alignment [#147]

--- app/models/market_files/free_form_widget_market.rb
+++ app/models/market_files/free_form_widget_market.rb
@@ -1,7 +1,7 @@
 module FreeFormWidgetMarket
-  THUMBNAIL_SIZES = {:widget_square => '238x238#',
-                     :widget_wide_2 => '483x238#',
-                     :widget_wide_3 => '729x238#',
-                     :widget_wide_4 => '975x238#',
+  THUMBNAIL_SIZES = {:widget_square => '216x238#',
+                     :widget_wide_2 => '462x238#',
+                     :widget_wide_3 => '708x238#',
+                     :widget_wide_4 => '954x238#',
                      :widget_tall => '238'}.merge Widget::THUMBNAIL_SIZES
 end
