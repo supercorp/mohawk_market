3e1bdd2b014ea5d4753aabf2a8b309b1aa595ab4
Randy J. Hunt <randy@citizenscholar.com>
2010-01-03 22:11:39 -0500

    revised freeform widget image sizes, helpcopy and some clases to propperly align and size images in the context of other widget types [#147 state:deploy]

--- app/models/market_files/free_form_widget_market.rb
+++ app/models/market_files/free_form_widget_market.rb
@@ -1,12 +1,12 @@
 module FreeFormWidgetMarket
-  THUMBNAIL_SIZES = {:widget_square => '216x238#',
-                     :widget_wide_2 => '462x238#',
-                     :widget_wide_3 => '708x238#',
-                     :widget_wide_4 => '954x238#',
+  THUMBNAIL_SIZES = {:widget_square => '216x247#',
+                     :widget_wide_2 => '462x247#',
+                     :widget_wide_3 => '708x247#',
+                     :widget_wide_4 => '954x247#',
                      :widget_tall => '216'}.merge Widget::THUMBNAIL_SIZES
-  IMAGE_SIZE_HELP_TEXT = {'square' => "216px x 238px",
+  IMAGE_SIZE_HELP_TEXT = {'square' => "216px x 247px",
                         'tall' => "216px wide and any height",
-                        'wide_2' => "462px x 238px",
-                        'wide_3' => "708px x 238px",
-                        'wide_4' => "954px x 238px"} 
+                        'wide_2' => "462px x 247px",
+                        'wide_3' => "708px x 247px",
+                        'wide_4' => "954px x 247px"} 
 end
