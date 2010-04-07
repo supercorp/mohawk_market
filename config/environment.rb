a93909e3e4c3e7ef6b1f15dc18c742e56d24602e
ryandeussing <ryandeussing@gmail.com>
2010-04-07 11:02:13 -0400

    ’out

--- config/environment.rb
+++ config/environment.rb
@@ -27,7 +27,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "RedCloth", :version => '4.1.1'
+  config.gem "RedCloth"
   config.gem 'tzinfo'
   config.gem 'uuid'
   config.gem 'json'
