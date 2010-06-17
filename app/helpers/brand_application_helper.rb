6cb06a16a9c906e0d3e547465c18ee4fa47d5ede
ryandeussing <ryandeussing@gmail.com>
2010-06-17 13:31:01 -0400

    modified footer text by request

--- app/helpers/brand_application_helper.rb
+++ app/helpers/brand_application_helper.rb
@@ -1,7 +1,7 @@
 module BrandApplicationHelper
   def initialize
-    @apply_to_sell_link_text = "I want to sell on Felt and Wire Shop."
-    @want_to_sell_not_logged_in_text = "Want to sell on Felt and Wire Shop?"
+    @apply_to_sell_link_text = "I want to sell on Felt &amp; Wire Shop."
+    @want_to_sell_not_logged_in_text = "Felt &amp; Wire Shop is a curated marketplace of designer papergoods. Whether you’re looking for a unique gift or elegant note cards, you’ll find everything from beautifully crafted letterpress cards and thank you notes to engraved social stationery. There’s also unique holiday gift wrap, handmade journals, and sketch books. Felt &amp; Wire Shop is also a great source for limited edition posters, prints and artists books that you won’t find anywhere else.<br /><br />The Felt &amp; Wire collective is made up of designers, printers, bookbinders, and artists. We’re always looking for spirited new work with a fresh sensibility.<br />"
     super
   end
 end
