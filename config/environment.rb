33921e426da4cbb82843fd1aa66a32c43d7359b6
Eric Draut <edraut@gmail.com>
2011-03-20 16:17:38 -0600

    added order pagination to curator dashboard

--- config/environment.rb
+++ config/environment.rb
@@ -31,7 +31,7 @@ Rails::Initializer.run do |config|
   config.gem 'tzinfo'
   config.gem 'uuid'
   config.gem 'json'
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gemcutter.org'
   config.gem 'gnumarcelo-campaigning', :lib => 'campaigning'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
