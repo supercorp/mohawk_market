673cf4b98e6b3c1fff73c9fdda0f18b40b2c7ab7
Eric Draut <edraut@gmail.com>
2012-09-08 09:20:58 -0600

    deleting a user now deletes all associated messages. Fixed pardot seller email list addition

--- app/models/market_files/seller_market.rb
+++ app/models/market_files/seller_market.rb
@@ -5,10 +5,14 @@ module SellerMarket
   FEATURED_PRODUCT_LIMIT = FEATURED_PRODUCT_ROW_LIMIT * (MARKET_UNITS_PER_ROW - 1)
 
   def add_to_campaign_monitor_seller_list
-  	begin
-			client = Pardot::Client.new 'ryan@wearesupercorp.com', 'M0hawk123', 'ee83b9cc736657c1f6fde3c51ebb5d32'
-			prospect = client.prospects.create(self.email, :list_66391 => 1)
-		rescue
-		end
+    Rails.logger.info("Seller#add_to_campaign_monitor_seller_list: adding #{email}")
+    begin
+      client = Pardot::Client.new 'ryan@wearesupercorp.com', 'M0hawk123', 'ee83b9cc736657c1f6fde3c51ebb5d32'
+      prospect = client.prospects.update_by_email(email, :list_66391 => 1)
+    rescue Exception => e
+      Rails.logger.error("Seller#add_to_campaign_monitor_seller_list: error adding #{email}")
+      Rails.logger.error(e.message)
+      Rails.logger.error(e.backtrace.join("\n"))
+    end
 	end
 end
--- app/models/market_files/user_market.rb
+++ app/models/market_files/user_market.rb
@@ -3,7 +3,9 @@ module UserMarket
 		begin
 			client = Pardot::Client.new 'ryan@wearesupercorp.com', 'M0hawk123', 'ee83b9cc736657c1f6fde3c51ebb5d32'
 			prospect = client.prospects.create(self.email, :list_65071 => 1)
-		rescue
+		rescue Exception => e
+			Rails.logger.error("User#add_subscriber: #{e.message}")
+			Rails.logger.error(e.backtrace.join("\n"))
 		end
 	end
 end
